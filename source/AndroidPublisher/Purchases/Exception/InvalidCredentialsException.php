bfdaae8efb3d6874d2a313a4619ad5a39c73ae81
Alexey Maslov <alexey.y.maslov@gmail.com>
2015-04-19 22:58:59 +0300

    Added InvalidCredentialsException for AndroidPublisher

---
+++ source/AndroidPublisher/Purchases/Exception/InvalidCredentialsException.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ */
+
+namespace alxmsl\Google\AndroidPublisher\Purchases\Exception;
+
+/**
+ * Exception for invalid authorization credentials
+ * @author alxmsl
+ */
+final class InvalidCredentialsException extends ErrorException {}
